5119701c8f3572ff3249ca03cee55b4e99497903
m4jidRafiei <m4jid.rafiei@gmail.com>
2021-06-02 09:58:07 +0200

    version 0.0.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     install_requires=[
         'pylcs==0.0.6',
         'pandas>=0.24.2',
-        'numpy>=1.18.2',
+        'numpy>=1.18.1',
         'pm4py==1.2.10'
     ],
     project_urls={
